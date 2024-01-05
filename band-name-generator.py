e96be07edf9deff9762e6fef9111363c1e2ae15d
campuedfranz <campuedfranz@gmail.com>
2024-01-05 17:20:57 +0800

    day 1 of learning python

---
+++ band-name-generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Ultimate Band Name Generator?")
+
+city_address = input("In what city did you grew up?\n")
+
+pet_name = input("What is the name of your pet?\n")
+
+print("Ultimate Band Name: " + city_address + " " + pet_name)
---
+++ main.py
@@ -0,0 +1,27 @@
+#Printing to the console in Python
+print("Hello World!")
+
+#\n next line
+print("Hello World!\nHello World!")
+
+#Concatenating strings
+print("Hello" + " " + "Franz!")
+
+#Input function
+input("What is your name?")
+
+#Input gets executed before print since its inside print
+print("My age is " + input("What is your age?"))
+
+#Length function
+print(len(input("Type any word to determine it's character lenght")))
+
+#variables
+my_name = "Franz"
+my_age = 26
+
+print(my_name)
+
+my_name = "Karla"
+
+print(my_name)
